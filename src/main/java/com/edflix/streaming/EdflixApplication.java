02d55670b7a7971f07e58c85249fa33e023c65f3
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-07-08 01:29:25 -0300

    created project

---
+++ src/main/java/com/edflix/streaming/EdflixApplication.java
@@ -0,0 +1,13 @@
+package com.edflix.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdflixApplication.class, args);
+	}
+
+}
